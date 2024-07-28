c01bbf6353bef40db7adcac7c725b74bbcd81dac
shaoxiaodan <sandays_cn@163.com>
2024-07-27 17:33:56 -0700

    Pet的三层搭建
    1，controller
    2，service
    3，serviceImpl

---
+++ src/main/java/com/example/demo/controller/PetController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.service.PetService;
+
+@RestController
+@RequestMapping("/pet")
+public class PetController {
+
+	@Autowired
+	private PetService petService;
+}
---
+++ src/main/java/com/example/demo/service/impl/PetServiceImpl.java
@@ -0,0 +1,10 @@
+package com.example.demo.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.service.PetService;
+
+@Service
+public class PetServiceImpl implements PetService {
+
+}
