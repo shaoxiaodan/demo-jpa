dd5a79e56634fe9fa12635481e401ddb8d22f96d
shaoxiaodan <sandays_cn@163.com>
2024-07-27 17:35:22 -0700

    student实体类的表映射配置

---
+++ src/main/java/com/example/demo/domain/Student.java
@@ -0,0 +1,43 @@
+package com.example.demo.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/*
+ * 外键表：子表
+ * 外键列=主键表的主键字段cid
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity(name = "t_student")
+public class Student {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int sid;
+
+	@Column
+	private String sName;
+
+	@Column
+	private int age;
+
+	@Column
+	private String description;
+
+	@ManyToOne
+	@JoinColumn(name = "cid") // 增加（外键）列，如果不指定name属性，则为clazz
+	private Clazz clazz; // 多个学生对象属于同一个班级
+
+}
