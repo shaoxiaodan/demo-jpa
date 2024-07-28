c54cf65d0b9f59e7736c7b7a6110c9c016c0a9d4
shaoxiaodan <sandays_cn@163.com>
2024-07-28 09:11:18 -0700

    girl实体类的表映射关系配置

---
+++ src/main/java/com/example/demo/domain/Girl.java
@@ -0,0 +1,32 @@
+package com.example.demo.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity(name = "t_girl")
+public class Girl {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int gId;
+
+	@Column
+	private String gName;
+
+	// 任何一方建立外键维护都可以，
+	// 因为one to one关系，双方关系对等
+	// 当前的Girl来mappedBy，也就是放弃外键维护权利(Boy来维护外键)
+	@OneToOne(mappedBy = "girl")
+	private Boy boy;
+}
